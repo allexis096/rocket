a8ce091e0cca208ad2df72393812bb791a09419a
allexis <allexissoares@gmail.com>
2021-02-28 12:46:10 -0300

    feat: added GlobalStyles and ThemeProvider

---
+++ src/styles/global.ts
@@ -0,0 +1,41 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  @media (max-width: 1080px) {
+    html {
+      font-size: 93.75%;
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%;
+    }
+  }
+
+  body {
+    background: ${({ theme }) => theme.background};
+    color: ${({ theme }) => theme.white};
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem 'Roboto', sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export default GlobalStyles;
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,6 @@
+const theme = {
+  background: 'rgb(18, 18, 20)',
+  white: '#fff',
+};
+
+export default theme;
