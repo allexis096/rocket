92d2363a38f3159d14229d7234abe9d2dabc2ca7
allexis <allexissoares@gmail.com>
2021-02-28 14:04:42 -0300

    chore: svg configs

--- @types/styled.d.ts
+++ @types/styled.d.ts
@@ -3,6 +3,10 @@ import 'styled-components';
 declare module 'styled-components' {
   export interface DefaultTheme {
     background: string;
+    main: string;
     white: string;
+    primary: string;
+    rocket: string;
+    button: string;
   }
 }
