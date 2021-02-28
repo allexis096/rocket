1c9d5e325739e4c1b29adac0c3f6f7ed1d558fd5
allexis <allexissoares@gmail.com>
2021-02-28 18:25:41 -0300

    feat: add cardCourses component

---
+++ src/components/cardCourses.tsx
@@ -0,0 +1,20 @@
+import * as S from '../styles/components/cardCourses';
+
+interface ICardProps {
+  image: React.ReactNode;
+  text: string;
+  name: 'discover' | 'gostack';
+}
+
+export default function CardCourses({ image, text, name }: ICardProps) {
+  return (
+    <S.Container name={name}>
+      <S.Card>
+        <S.Content>
+          {image}
+          <p>{text}</p>
+        </S.Content>
+      </S.Card>
+    </S.Container>
+  );
+}
